d28706719d7d8e805ea4cfc8132cbd16b694c63c
wasdn10 <naimwanomar10@gmail.com>
2024-12-01 23:06:39 +0800

    Delete features directory

--- features/testimonials.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-print("Content-Type: text/html\n")
-print("""
-<ul>
-    <li>"Fantastic service and support!" - Client A</li>
-    <li>"Highly professional team." - Client B</li>
-</ul>
-""")
